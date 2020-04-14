c10e74fd75e4cd09c44e55fc59f8784d2e771716
Yanxin <42981100+yanxin0124@users.noreply.github.com>
2020-04-14 14:31:56 -0400

    more

--- a-patch-of-happy/background.js
+++ a-patch-of-happy/background.js
@@ -28,7 +28,9 @@ let morning13 = "Drink more water!";
 let morning14 = "I think you're just so incredible";
 let morning15 = "All dolled up and yeti to go!";
 let morning16 = "Let's be productive today!";
-let morning = [morning1, morning2, morning3, morning4, morning5, morning6, morning7, morning8, morning9, morning10, morning11, morning12, morning13, morning14, morning15, morning16];
+let morning17 = "I'm here for you!";
+let morning18 = "Hop, skip, jump!";
+let morning = [morning1, morning2, morning3, morning4, morning5, morning6, morning7, morning8, morning9, morning10, morning11, morning12, morning13, morning14, morning15, morning16, morning17, morning18];
 
 let noon1 = "Good afternoon Nick!";
 let noon2 = "Don't forget to stay hydrated!";
@@ -39,12 +41,15 @@ let noon6 = "Lunch time!";
 let noon7 = "hungry. want food.";
 let noon8 = "I hope your day is going well!";
 let noon9 = "Be productive!";
-let noon10 = "Bzzt!";
+let noon10 = "Bzzzt!";
 let noon11 = "Hi best friend!";
 let noon12 = "I love you so much!";
 let noon13 = "You make me happier than Lily";
 let noon14 = "Your jokes are so funny bro";
-let noon = [noon1, noon2, noon3, noon4, noon5, noon6, noon7, noon8, noon9, noon10, noon11, noon12, noon13, noon14];
+let noon15 = "You're my otter half";
+let noon16 = "You make me happy!";
+let noon17 = "Drink more water!";
+let noon = [noon1, noon2, noon3, noon4, noon5, noon6, noon7, noon8, noon9, noon10, noon11, noon12, noon13, noon14, noon15, noon16, noon17];
 
 let night1 = "How'd your day go?";
 let night2 = "Procrastination is bad";
@@ -54,14 +59,22 @@ let night5 = "Your Yasuo is so clean bro";
 let night6 = "You make me happier than Lily";
 let night7 = "Do the filo thing!";
 let night8 = "Heyyy you come here often?";
-let night = [night1, night2, night3, night4, night5, night6, night7, night8];
+let night9 = "You make me happy!"
+let night10 = "Remember to drink water!"
+let night11 = "Slouching is bad for your posture!";
+let night12 = "You're my best friend!";
+let night13 = "Bert!";
+let night14 = "Never look a tulip in the eye...";
+let night = [night1, night2, night3, night4, night5, night6, night7, night8, night9, night10, night11, night12, night13, night14];
 
 let dinner1 = "Did you eat yet?";
 let dinner2 = "Dindin time!";
 let dinner3 = "Feed meeee";
-let dinner4 = "I love you more I love eggs";
+let dinner4 = "I love you more than I love eggs";
 let dinner5 = "Really craving some fiesta lime chicken right now";
-let dinner = [dinner1, dinner2, dinner3, dinner4, dinner5];
+let dinner6 = "Let's be productive!";
+let dinner7 = "Drink water!";
+let dinner = [dinner1, dinner2, dinner3, dinner4, dinner5, dinner6, dinner7];
 
 let sleep1 = "You should sleep soon!";
 let sleep2 = "Can't wait to see you again tomorrow!";
@@ -69,7 +82,8 @@ let sleep3 = "Nighty night!";
 let sleep4 = "Rest well!";
 let sleep5 = "Sweet dreams!";
 let sleep6 = "I love you so much!";
-sleep = [sleep1, sleep2, sleep3, sleep4, sleep5, sleep6];
+let sleep7 = "When was the last time you changed your contacts?";
+sleep = [sleep1, sleep2, sleep3, sleep4, sleep5, sleep6, sleep7];
 
 let late1 = "You should be sleeping!";
 let late2 = "What's keeping you up?";
@@ -112,20 +126,41 @@ function setup() {
   else if (time >= 20 && time < 22) { // 8pm - 10pm
     greeting = night[getRandom(night.legnth)]
   }
-  else if (time >= 22 && time < 24) { // 10pm - 4pm
+  else if (time >= 22 && time <= 23) { // 10pm - 12pm
     greeting = sleep[getRandom(sleep.legnth)]
   }
+  else if (time >= 0 && time < 4) { // 1am - 4am
+    greeting = late[getRandom(late.legnth)]
+  }
 
-  let background1 = "images/otter1.jpg";
-  let background2 = "images/otter2.jpg";
-  let background = [background1, background2];
+  let bg1 = "images/otter1.jpg";
+  let bg2 = "images/otter2.jpg";
+  let bg3 = "images/filo1.gif";
+  let bg4 = "images/filo2.gif";
+  let bg5 = "images/ferret1.jpg";
+  let bg6 = "images/duck1.jpg";
+  let bg7 = "images/ferret2.jpg";
+  let bg8 = "images/otter3.jpg";
+  let bg9 = "images/otter4.jpg";
+  let bg10 = "images/bee1.png";
+  let bg11 = "images/dog1.jpg";
+  let bg12 = "images/dog2.jpg";
+  let bg13 = "images/dog3.jpg";
+  let bg14 = "images/dog4.jpg";
+  let bg = [bg1, bg2, bg3, bg4, bg5, bg6, bg7, bg8, bg9, bg10, bg11, bg12, bg13, bg14];
 
   document.getElementById("greeting").innerHTML = greeting;
-  document.body.style.backgroundImage = "url('" + background[getRandom(background.length)] + "')";
+  document.body.style.backgroundImage = "url('" + bg[getRandom(bg.length)] + "')";
 
 }
 
 function draw() {
+
+  let hours = hour();
+
+  if (hour() > 12) {
+    hours -= 12;
+  }
 
   let minutes = minute();
 
@@ -160,7 +195,6 @@ function draw() {
     minutes = "09";
   }
 
-  document.getElementById("time").innerHTML = hour() + ":" + minutes;
-
+  document.getElementById("time").innerHTML = hours + ":" + minutes;
 
 }
